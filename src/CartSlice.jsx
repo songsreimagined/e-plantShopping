9ec889b1f51eed42ce2e275e2950dfb84a47e6af
songsreimagined <reimaginedsongs@gmail.com>
2025-01-22 08:25:12 -0500

    initial commit

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,16 +4,40 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    numOfItems: 0
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+          existingItem.quantity++;
+        } else {
+          state.items.push({ name, image, cost, quantity: 1 });
+        }
+
+        state.numOfItems += 1;
     },
     removeItem: (state, action) => {
+        const { name, quantity } = action.payload;
+        state.items = state.items.filter(item => item.name !== name);
+
+        if (state.numOfItems < 0) {
+            state.numOfItems = 0;
+        }
+        else {
+            state.numOfItems -= quantity;
+        }
     },
     updateQuantity: (state, action) => {
+        const { name, quantity } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
 
-    
+        if (existingItem) {
+            const differenceQuantity = quantity - existingItem.quantity;
+            state.numOfItems += differenceQuantity;
+            existingItem.quantity = quantity;
+        }
     },
   },
 });
